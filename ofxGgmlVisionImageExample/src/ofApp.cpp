3b68015b9e898d87e2282e52b981c1a6e4c1bc45
Jonathan Frank <JonathanFrank@gmx.net>
2026-05-16 02:18:53 +0200

    Use ImGui in image example

--- ofxGgmlVisionImageExample/src/ofApp.cpp
+++ ofxGgmlVisionImageExample/src/ofApp.cpp
@@ -2,6 +2,7 @@
 
 void ofApp::setup() {
 	ofSetWindowTitle("ofxGgmlVision smoke example");
+	gui.setup(nullptr, false);
 	request.imagePath = "images/frame.png";
 	status = ofxGgmlVisionUtils::describe(request);
 	ofLogNotice("ofxGgmlVisionImageExample") << status;
@@ -9,7 +10,15 @@ void ofApp::setup() {
 
 void ofApp::draw() {
 	ofBackground(18);
-	ofSetColor(240);
-	ofDrawBitmapString("ofxGgmlVision", 32, 48);
-	ofDrawBitmapString(status, 32, 78);
+	gui.begin();
+	ImGui::SetNextWindowPos(ImVec2(24.0f, 24.0f), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(560.0f, 220.0f), ImGuiCond_Once);
+	if (ImGui::Begin("ofxGgmlVision Image Example")) {
+		ImGui::TextUnformatted("Image Request");
+		ImGui::Separator();
+		ImGui::TextWrapped("%s", status.c_str());
+	}
+	ImGui::End();
+	gui.end();
+	gui.draw();
 }
